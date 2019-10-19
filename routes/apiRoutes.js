d7f8eb4586bb8e92942028e96feb6008da34d469
rb2001 <rbaird2001@yahoo.com>
2019-10-19 03:13:52 -0600

    Added promises to queries and finalized request post route

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,11 +5,47 @@ var db = require("../config/orm");
 module.exports = function(app) {
   // Get all examples
   app.post("/slkdsk", function(req, res) {
-    res.send(db.createRequest(req.body));
-    //  {
-    //   function(res){console.log(res)}; //res.json(dbExamples);
-    // });
+    var newRequestId = 0;
+    db.createRequest(req.body)
+      .then(function(createReqResp) {
+        newRequestId = createReqResp;
+        db.getSingleRecord(createReqResp)
+          .then(function(dataset) {
+            diaryEntry = {
+              requestid: dataset[0].id,
+              entryType: "New Request",
+              diaryText: dataset[0].initialDescription,
+              time: dataset[0].time
+            };
+            console.log(diaryEntry);
+            db.createDiary(diaryEntry)
+              .then(function() {
+                //console.log(createDiaryResp);
+                res.send("Request created with id# " + newRequestId);
+              })
+              .catch(function(err) {
+                console.log("Create New Diary Error");
+                console.log(err);
+                res.send("Create Request Error at Diary");
+              });
+          })
+          .catch(function(err) {
+            console.log("Diary Prep Error");
+            console.log(err);
+            res.send("Create Request Error at Diary Prep");
+          });
+      })
+      .catch(function(err) {
+        console.log(err);
+        res.send("Create Request Error at Requests");
+      });
   });
+
+  //res.send(db.createRequest(req.body));
+  //  {
+  //   function(res){console.log(res)}; //res.json(dbExamples);
+  // });
+
 
   // Create a new example
   // app.post("/slkdsk", function(req, res) {
