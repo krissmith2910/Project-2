b37ba9e5592ddc4b47bdbefd2f339192a1c54e64
rb2001 <rbaird2001@yahoo.com>
2019-10-20 20:54:36 -0600

    testing Slack Challenge Failure

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,55 +5,59 @@ var db = require("../config/orm");
 module.exports = function(app) {
   // Get all examples
   app.post("/desk/requests", function(req, res) {
-    var newRequestId = 0;
-    if(req.body.challenge !== undefined){
-      console.log(req.body);
-      res.send(req.body.challenge);
-      return ("challenge");
-    }
+    //var newRequestId = 0;
     console.log(req.body);
-    console.log("\n");
-    console.log(req.body.event_time);
-    console.log("\n");
-    let newReq = {
-      slackID: req.body.event.client_msg_id,
-      requester: req.body.event.user,
-      initialDescription: req.body.event.text,
-      time: req.body.event_time
-    };
-    db.createRequest(newReq)
-      .then(function(createReqResp) {
-        newRequestId = createReqResp;
-        db.getSingleRecord(createReqResp)
-          .then(function(dataset) {
-            diaryEntry = {
-              requestid: dataset[0].id,
-              entryType: "New Request",
-              diaryText: dataset[0].initialDescription,
-              time: dataset[0].time
-            };
-            console.log(diaryEntry);
-            db.createDiary(diaryEntry)
-              .then(function() {
-                //console.log(createDiaryResp);
-                res.send("Request created with id# " + newRequestId);
-              })
-              .catch(function(err) {
-                console.log("Create New Diary Error");
-                console.log(err);
-                res.send("Create Request Error at Diary");
-              });
-          })
-          .catch(function(err) {
-            console.log("Diary Prep Error");
-            console.log(err);
-            res.send("Create Request Error at Diary Prep");
-          });
-      })
-      .catch(function(err) {
-        console.log(err);
-        res.send("Create Request Error at Requests");
-      });
+    console.log("end of body \n");
+    console.log(res);
+    return true;
+    // if(req.body.challenge !== undefined){
+    //   console.log(req.body);
+    //   res.send(req.body.challenge);
+    //   return ("challenge");
+    // }
+    // console.log(req.body);
+    // console.log("\n");
+    // console.log(req.body.event_time);
+    // console.log("\n");
+    // let newReq = {
+    //   slackID: req.body.event.client_msg_id,
+    //   requester: req.body.event.user,
+    //   initialDescription: req.body.event.text,
+    //   time: req.body.event_time
+    // };
+    // db.createRequest(newReq)
+    //   .then(function(createReqResp) {
+    //     newRequestId = createReqResp;
+    //     db.getSingleRecord(createReqResp)
+    //       .then(function(dataset) {
+    //         diaryEntry = {
+    //           requestid: dataset[0].id,
+    //           entryType: "New Request",
+    //           diaryText: dataset[0].initialDescription,
+    //           time: dataset[0].time
+    //         };
+    //         console.log(diaryEntry);
+    //         db.createDiary(diaryEntry)
+    //           .then(function() {
+    //             //console.log(createDiaryResp);
+    //             res.send("Request created with id# " + newRequestId);
+    //           })
+    //           .catch(function(err) {
+    //             console.log("Create New Diary Error");
+    //             console.log(err);
+    //             res.send("Create Request Error at Diary");
+    //           });
+    //       })
+    //       .catch(function(err) {
+    //         console.log("Diary Prep Error");
+    //         console.log(err);
+    //         res.send("Create Request Error at Diary Prep");
+    //       });
+    //   })
+    //   .catch(function(err) {
+    //     console.log(err);
+    //     res.send("Create Request Error at Requests");
+    //   });
   });
   app.get("/",function(req, res) {
     res.send("access achieved");
