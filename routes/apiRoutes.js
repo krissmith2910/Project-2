4468fc9b2667ef05cde1b9bd5651df1c68115b2b
Zer0 <lucismcg@gmail.com>
2019-10-21 21:12:41 -0600

    updated html with handlebars

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,29 @@
-var db = require("../models");
+//var db = require("../models");
+
+var db = require("../config/orm");
 
 module.exports = function(app) {
   // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  app.post("/slkdsk", function(req, res) {
+    res.send(db.createRequest(req.body));
+    //  {
+    //   function(res){console.log(res)}; //res.json(dbExamples);
+    // });
   });
 
   // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.post("/slkdsk", function(req, res) {
+  //   db.Example.create(req.body).then(function(dbExample) {
+  //     res.json(dbExample);
+  //   });
+  // });
 
   // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
-  });
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(
+  //     dbExample
+  //   ) {
+  //     res.json(dbExample);
+  //   });
+  // });
 };
